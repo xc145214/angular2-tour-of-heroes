3452071c8f06be6fd03a9fc6a8bb6340101af3dd
xc145214 <xc145214@gmail.com>
2016-07-06 12:44:46 +0800

    http

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,44 +1,49 @@
-/**
- * Created by Administrator on 2016/7/5.
- */
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, OnDestroy, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-
-import {Hero} from "./hero";
-import {HeroService} from "./hero.service";
-
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
 @Component({
-    selector:'my-hero-detail',
+    selector: 'my-hero-detail',
     templateUrl: 'app/hero-detail.component.html',
+    styleUrls: ['app/hero-detail.component.css']
 })
-
-export class HeroDetailComponent implements OnInit,OnDestroy{
-
-    hero:Hero;
-    sub:any;
-
+export class HeroDetailComponent implements OnInit, OnDestroy {
+    @Input() hero: Hero;
+    @Output() close = new EventEmitter();
+    error: any;
+    sub: any;
+    navigated = false; // true if navigated here
     constructor(
-        private heroService:HeroService,
-        private route:ActivatedRoute
-    ){}
-
-
+        private heroService: HeroService,
+        private route: ActivatedRoute) {
+    }
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
-            let id = +params['id'];
-            this.heroService.getHero(id)
-                .then(hero => this.hero = hero);
+            if (params['id'] !== undefined) {
+                let id = +params['id'];
+                this.navigated = true;
+                this.heroService.getHero(id)
+                    .then(hero => this.hero = hero);
+            } else {
+                this.navigated = false;
+                this.hero = new Hero();
+            }
         });
     }
-
-
     ngOnDestroy() {
         this.sub.unsubscribe();
     }
-
-    goBack() {
-        window.history.back();
+    save() {
+        this.heroService
+            .save(this.hero)
+            .then(hero => {
+                this.hero = hero; // saved hero, w/ id if new
+                this.goBack(hero);
+            })
+            .catch(error => this.error = error); // TODO: Display error message
     }
-
+    goBack(savedHero: Hero = null) {
+        this.close.emit(savedHero);
+        if (this.navigated) { window.history.back(); }
+    }
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -11,20 +11,51 @@ import {HeroService} from "./hero.service";
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
-    styleUrls:  ['app/heroes.component.css']
+    styleUrls:  ['app/heroes.component.css'],
+    directives:[HeroDetailComponent]
 
 })
 export class HeroesComponent implements OnInit{
     heroes:Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error:any;
 
     constructor(
         private router:Router,
         private heroService:HeroService){}
 
     getHeroes() {
-        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+        this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes)
+            .catch(error => this.error = error);
     }
+
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+
+
+    deleteHero(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+            .catch(error => this.error = error);
+    }
+
+
 
     ngOnInit() {
         this.getHeroes();
@@ -32,6 +63,7 @@ export class HeroesComponent implements OnInit{
 
     onSelect(hero: Hero) {
         this.selectedHero = hero;
+        this.addingHero = false;
     }
 
     gotoDetail() {
